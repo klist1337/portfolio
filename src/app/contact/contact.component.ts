b47610ebc5e04fbd90d92169774bc8840d8fd6ce
klist1337 <meneur2017@gmail.com>
2024-02-05 20:42:10 +0100

    deployment

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NgClass } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { DarkModeService } from 'angular-dark-mode';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
-export class ContactComponent {
-
+export class ContactComponent implements OnInit {
+  isDarkMode = false;
+  darkMode$ = this.darkModeService.darkMode$;
+  constructor(private darkModeService:DarkModeService) {}
+  ngOnInit(): void {
+    this.darkModeService.darkMode$.subscribe(
+      (darkmode) => {
+        this.isDarkMode = darkmode;
+      });
+  }
+  getContactStyle(): {[key:string]: boolean} {
+    return {
+      'dark-mode': this.isDarkMode,
+    }
+  }
 }
